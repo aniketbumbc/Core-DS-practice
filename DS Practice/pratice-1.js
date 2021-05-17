bf6a5d20fd4e3f0fe1b37af908b1cd64899460f2
aniket <aniket.umbc@gmail.com>
2021-05-17 07:25:22 +0530

    Frequency count comprise

--- DS Practice/pratice-1.js
+++ DS Practice/pratice-1.js
@@ -15,4 +15,36 @@ function subArray(arr, num) {
   return maxSum;
 }
 
-console.log(subArray([1, 2, 3, 4, 5], 2));
+// console.log(subArray([1, 2, 3, 4, 5], 2));
+
+/**
+ *  Check two number same fequency of degit
+ */
+
+function checkFeq(num1, num2) {
+  let numObj1 = makeObje(num1);
+  let numObj2 = makeObje(num2);
+
+  for (let num in numObj1) {
+    if (numObj2[num] !== numObj1[num]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function makeObje(num) {
+  let temp = num.toString().split('');
+  let num1Obj = {};
+  for (let num of temp) {
+    if (num1Obj[num] !== undefined) {
+      num1Obj[num]++;
+    } else {
+      num1Obj[num] = 1;
+    }
+  }
+
+  return num1Obj;
+}
+
+console.log(checkFeq(22, 32));
--- dist/pratice-1.e2f54c0b.js
+++ dist/pratice-1.e2f54c0b.js
@@ -118,6 +118,12 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   return newRequire;
 })({"DS Practice/pratice-1.js":[function(require,module,exports) {
+function _createForOfIteratorHelper(o, allowArrayLike) { var it = typeof Symbol !== "undefined" && o[Symbol.iterator] || o["@@iterator"]; if (!it) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = it.call(o); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it.return != null) it.return(); } finally { if (didErr) throw err; } } }; }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
 function subArray(arr, num) {
   var maxSum = 0;
   var tempSum = 0;
@@ -134,9 +140,53 @@ function subArray(arr, num) {
   }
 
   return maxSum;
+} // console.log(subArray([1, 2, 3, 4, 5], 2));
+
+/**
+ *  Check two number same fequency of degit
+ */
+
+
+function checkFeq(num1, num2) {
+  var numObj1 = makeObje(num1);
+  var numObj2 = makeObje(num2);
+
+  for (var num in numObj1) {
+    if (numObj2[num] !== numObj1[num]) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
-console.log(subArray([1, 2, 3, 4, 5], 2));
+function makeObje(num) {
+  var temp = num.toString().split('');
+  var num1Obj = {};
+
+  var _iterator = _createForOfIteratorHelper(temp),
+      _step;
+
+  try {
+    for (_iterator.s(); !(_step = _iterator.n()).done;) {
+      var _num = _step.value;
+
+      if (num1Obj[_num] !== undefined) {
+        num1Obj[_num]++;
+      } else {
+        num1Obj[_num] = 1;
+      }
+    }
+  } catch (err) {
+    _iterator.e(err);
+  } finally {
+    _iterator.f();
+  }
+
+  return num1Obj;
+}
+
+console.log(checkFeq(22, 32));
 },{}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
